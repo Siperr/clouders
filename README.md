a2a2ff174a72241394240788f728616109520a00
Siperr <boreale72@gmail.com>
2026-09-22 12:31:29 +0200

    fix: better navigability for shared folder

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ Lightweight google drive clone built with Express and EJS (SSR).
 # TODO 
 - [ ] add shared folder feature
     - notification centre to accept an invite to share a folder?
-- [ ] add file operations permissions
-- [ ] add path to all folders and in shared folder display permission and folder owner
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const { prisma, Permission } = require("../config/prisma");
-const { Prisma } = require("@prisma/client");
+const { getEffectivePermission } = require("../lib/queries");
 
 function verifyAuth(req, res, next) {
   if (req.isAuthenticated()) {
@@ -9,61 +9,21 @@ function verifyAuth(req, res, next) {
   res.redirect("/login");
 }
 
-async function getEffectivePermission(folderId, userId) {
-  const result =
-    await prisma.$queryRaw(Prisma.sql`WITH RECURSIVE folder_tree AS (
-
-        SELECT
-          id,
-          parent_folder_id,
-          0 AS depth
-        FROM folders
-        WHERE id = ${folderId}
-
-        UNION ALL
-
-        SELECT
-          f.id,
-          f.parent_folder_id,
-          ft.depth + 1
-        FROM folders f
-        JOIN folder_tree ft
-          ON f.id = ft.parent_folder_id
-      )
-
-      SELECT sf.permission
-      FROM folder_tree ft
-      JOIN shared_folders sf
-        ON sf.folder_id = ft.id
-      WHERE sf.user_id = ${userId}
-      ORDER BY ft.depth
-      LIMIT 1;
-    `);
-
-  return result[0]?.permission ?? null;
-}
-
 async function checkPermission(req, res, next) {
   try {
-    if(req.params.id && req.baseUrl.includes("file")) {
+    if (req.params.id && req.baseUrl.includes("file")) {
       const file = await prisma.files.findUnique({
         where: {
           id: Number(req.params.id),
         },
       });
-      req.file = {parent: file.folder_id};
+      req.file = { parent: file.folder_id };
     }
 
     const folderId = req.file ? Number(req.file.parent) : Number(req.params.id);
-    const shared_folder = await getEffectivePermission(folderId, Number(req.user.id));
-
-    console.log(
-      "checkPermission, req.id: ",
-      req.params.id,
-      ", req.user.id: ",
-      req.user.id,
-      ", shared_folder: ",
-      shared_folder
+    const shared_folder = await getEffectivePermission(
+      folderId,
+      Number(req.user.id),
     );
 
     
@@ -84,14 +44,14 @@ async function checkPermission(req, res, next) {
       },
     });
 
-    if(!folder && !shared_folder) {
+    if (!folder && !shared_folder) {
       throw new Error("User does not have permission to access this folder");
     }
 
     const permission = folder
-        ? Permission.OWNER
-        : shared_folder
-      ? shared_folder
+      ? Permission.OWNER
+      : shared_folder
+        ? shared_folder
         : null;
 
     req.permission = permission;
