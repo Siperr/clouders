24a5c2c92c48d3df7c1b7b7a0d89e15f25333b66
Siperr <boreale72@gmail.com>
2026-09-23 11:45:49 +0200

    fix: add regex to validate folder and file names

--- middlewares/formValidation.js
+++ middlewares/formValidation.js
@@ -3,18 +3,20 @@ const {body} = require('express-validator');
 const validateSignupForm = [
     body('username').
     notEmpty().withMessage('Username cannot be empty').
-    isAlphanumeric().
+    isAlphanumeric().withMessage('Username must be alphanumeric').
     isLength({min: 4, max: 100}),
     body('password').
     notEmpty().withMessage('Password cannot be empty').
-    isAlphanumeric()
+    isAlphanumeric().withMessage('Password must be alphanumeric')
 ];
 
 const validateRenameFolder = [
     body('new-name').
     notEmpty().withMessage('Folder name cannot be empty').
     isLength({min: 1, max: 100}).
-    isAlphanumeric().withMessage('Folder name must be alphanumeric')
+    trim().
+    matches(/^[a-zA-Z0-9 _-]+$/).
+    withMessage('Folder name must be alphanumeric and can contain spaces, underscores, and hyphens')
 ];
 
 
@@ -22,7 +24,9 @@ const validateRenameFile = [
     body('new-file-name').
     notEmpty().withMessage('File name cannot be empty').
     isLength({min: 1, max: 100}).
-    isAlphanumeric().withMessage('File name must be alphanumeric')
+    trim().
+    matches(/^[a-zA-Z0-9 _-]+$/).
+    withMessage('File name must be alphanumeric and can contain spaces, underscores, and hyphens')
 ];
 
 const validateShareFolder = [
